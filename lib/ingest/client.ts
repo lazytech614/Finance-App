a4085eadfbef620c9530f267c3c26c33ad25c0e6
lazytech614 <derupanjan2021@gmail.com>
2026-03-25 02:26:01 +0530

    update

--- lib/ingest/client.ts
+++ lib/ingest/client.ts
@@ -4,6 +4,8 @@ import { Inngest } from "inngest";
 export const inngest = new Inngest({ 
     id: "welth",
     name: "Welth",
+    // FOR PRODUCTION
+    eventKey: process.env.INNGEST_EVENT_KEY,  
     retryFunction: async (attempt: number) => ({
         delay: Math.pow(2, attempt) * 1000,
         maxAttempts: 2
